a6c11b8a50462433cc13a68aae49728cdeef3e29
Anis Lazaar <anis00723@gmail.com>
2023-04-02 21:46:14 +0100

    :sparkles: add levels show it in hud

--- src/systems/end_turn.rs
+++ src/systems/end_turn.rs
@@ -5,10 +5,10 @@ use crate::prelude::*;
 #[read_component(Point)]
 #[read_component(Player)]
 #[read_component(AmuletOfYala)]
-pub fn end_turn(ecs: &SubWorld, #[resource] turn_state: &mut TurnState) {
+pub fn end_turn(ecs: &SubWorld, #[resource] turn_state: &mut TurnState, #[resource] map: &Map) {
   let mut player_hp = <(&Health, &Point)>::query().filter(component::<Player>());
   let mut amulet = <&Point>::query().filter(component::<AmuletOfYala>());
-  let current_state = turn_state.clone();
+  let current_state = *turn_state;
   let mut new_state = match current_state {
     TurnState::AwaitingInput => return,
     TurnState::PlayerTurn => TurnState::MonsterTurn,
@@ -25,6 +25,10 @@ pub fn end_turn(ecs: &SubWorld, #[resource] turn_state: &mut TurnState) {
     }
     if pos == amulet_pos {
       new_state = TurnState::Victory;
+    }
+    let idx = map.point2d_to_index(*pos);
+    if map.tiles[idx] == TileType::Exit {
+      new_state = TurnState::NextLevel;
     }
   });
 
